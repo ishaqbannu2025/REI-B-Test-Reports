c2c09b297b49fd4f0e2b0b9db285cb8161af5696
Muhammad Ishaq Khan <m.ishaqbannu@gmail.com>
2025-10-10 13:04:01 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3455683568-a1e1b",
-  "appId": "1:6088677777:web:a4db0045fe2ca240d6e230",
-  "apiKey": "AIzaSyDWNmFfXdOQVaGzblMjCnSMNeuXPKvt1fc",
-  "authDomain": "studio-3455683568-a1e1b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "6088677777"
+  apiKey: "AIzaSyDWNmFfXdOQVaGzblMjCnSMNeuXPKvt1fc",
+  authDomain: "studio-3455683568-a1e1b.firebaseapp.com",
+  projectId: "studio-3455683568-a1e1b",
+  storageBucket: "studio-3455683568-a1e1b.firebasestorage.app",
+  messagingSenderId: "6088677777",
+  appId: "1:6088677777:web:4c0c2b1496437ee7d6e230"
 };
